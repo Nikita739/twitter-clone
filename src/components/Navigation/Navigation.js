916fb2a0494bf3fae1ad703f8cdc54cf421a9890
Nikita739 <97597709+Nikita739@users.noreply.github.com>
2023-02-10 16:29:21 +0200

    Fixed login/registration section

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,68 +3,6 @@ import cl from './Navigation.module.css'
 import { useNavigate } from 'react-router-dom'
 import {AuthContext} from "../../App";
 
-// const Navigation = () => {
-//     let navigate = useNavigate()
-//
-//     const [isAuth] = useContext(AuthContext)
-//     const [active, setActive] = useState(0)
-//     const [user, setUser] = useState({})
-//
-//     useEffect(() => {
-//         if(isAuth) {
-//             const newUser = JSON.parse(atob(localStorage.getItem('token')))
-//             setUser(newUser)
-//         } else {
-//             setUser({username: "Not authorized"})
-//         }
-//     }, [])
-//
-//     let liArr = []
-//     if(isAuth) {
-//         liArr = [
-//             {name: "Home", path: "/"},
-//             {name: "Add post", path: "/new"},
-//             {name: "Logout", path: "/logout"}
-//         ]
-//     } else {
-//         liArr = [
-//             {name: "Home", path: "/"},
-//             {name: "Register", path: "/register"},
-//             {name: "Login", path: "/login"}
-//         ]
-//     }
-//
-//     const goTo = (path, index) => {
-//         setActive(index)
-//         navigate(path)
-//     }
-//
-//     return (
-//         <div className={cl.outer}>
-//             <ul>
-//                 {isAuth
-//                     ?
-//                         active === 1000
-//                             ?
-//                                 <li className={cl.active} onClick={() => goTo("/profile", 1000)}>{user.username}</li>
-//                             :
-//                                 <li onClick={() => goTo("/profile", 1000)}>{user.username}</li>
-//                     :
-//                         null
-//                 }
-//
-//                 {liArr.map((el, index) =>
-//                     active === index
-//                     ?
-//                         <li key={index} className={cl.active} onClick={() => goTo(el.path, index)}>{el.name}</li>
-//                     :
-//                         <li key={index} onClick={() => goTo(el.path, index)}>{el.name}</li>
-//                 )}
-//             </ul>
-//         </div>
-//     );
-// };
-
 const Navigation = () => {
     const [isAuth] = useContext(AuthContext)
     const [user, setUser] = useState({})
@@ -90,8 +28,6 @@ const Navigation = () => {
         if(isAuth) {
             const newUser = JSON.parse(atob(localStorage.getItem('token')))
             setUser(newUser)
-        } else {
-            setUser({username: "Not authorized"})
         }
     }, [])
 
